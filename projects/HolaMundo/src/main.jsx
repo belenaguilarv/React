1aa28b7b019a3c660aba13230efa21c15bef0853
belenaguilarv <belenaguiilarv@gmail.com>
2023-07-08 16:42:53 -0300

    Error en la ruta de la imagen. No puedo ver nada

--- projects/HolaMundo/src/main.jsx
+++ projects/HolaMundo/src/main.jsx
@@ -1,21 +1,10 @@
-/* eslint-disable react-refresh/only-export-components */
-/* eslint-disable react/prop-types */
-import React from 'react'
+/*import React from 'react'*/
 import ReactDOM from 'react-dom/client'
+import {App} from './App.jsx'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const Button = ({text}) =>{
-  return (
-    <button>{text}</button> 
-  )
-}
-
 root.render(
-  <React.Fragment>
-    <Button text="Button 1"/>
-    <Button text="Button 2"/>
-    <Button text="Button 3"/>
-  </React.Fragment>
+  <App />
 )
