5f2470ac14e2908e979b9c5da5d31ae1d13772a8
belenaguilarv <belenaguiilarv@gmail.com>
2023-07-08 18:55:57 -0300

    La foto ya se puede ver

--- projects/HolaMundo/src/App.jsx
+++ projects/HolaMundo/src/App.jsx
@@ -2,7 +2,7 @@ export function App() {
   return (
     <article>
       <header>
-      <img src="./images/Belu.png" alt="Foto perfil" />
+      <img src="../public/images/Belu.png" alt="Foto perfil" />
         <div>
           <strong>María Belén Aguilar Velásquez</strong>
           <span>@belenaguilarv</span>
